dc0b31060a17e63a5771f293fd0fc56b658f7720
khouloudAmamouHaddad <khouloud.amamouhaddad@gmail.com>
2023-02-10 15:21:21 +0100

    Login with JWT

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # manageRestaurant
+
+require('crypto').randomBytes(64).toString('hex'): generate 64 key for encryption/decryption
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const { query } = require('../connection');
 const connection = require('../connection');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
+//register
 router.post("/signup",(req,res)=>{
 
     let user = req.body;
-    query = "Select email, password, role, status from user where email=?";
-    connection.query(query, [user.email], (err,results)=>{
+    register_query = "Select email, password, role, status from user where email=?";
+    connection.query(register_query, [user.email], (err,results)=>{
 
         if(!err){
 
@@ -40,6 +44,46 @@ router.post("/signup",(req,res)=>{
     
     
 
-})
+});
+
+//login
+router.post("/login", (req,res)=>{
+
+    const user = req.body;
+
+    login_query= "SELECT email, password, role, status from user where email=?";
+
+    connection.query(login_query, [user.email], (err, results)=>{
+
+        if(!err){
+            if(results.length <= 0 || results[0].password !==user.password){
+
+                res.status(401).json({message: "Incorrect Username or Password"});
+
+            }else if(results[0].status === 'false'){
+
+                return res.status(401).json({message:"Wait for Admin Approval"});
+
+            }else if(results[0].password == user.password){
+
+                const response = {
+                    email: results[0].email,
+                    role: results[0].role
+                };
+                const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, {expiresIn:'8h'});
+
+                res.status(200).json({token: accessToken});
+
+            }else{
+                return res.status(400).json({message: "Something went wrong.Please try later"});
+            }
+        }else{
+
+            return res.status(500).json(err);
+
+        }
+
+    });
+});
 
 module.exports = router;
